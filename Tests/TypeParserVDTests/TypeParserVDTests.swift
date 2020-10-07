d33098d9573fa53f981ee3f1e8c879ceb686f59e
Victor Capilla <victorcapilladeveloper@gmail.com>
2020-10-07 22:15:13 +0200

    New method with result type as return and make the corresponding tests

--- Tests/TypeParserVDTests/TypeParserVDTests.swift
+++ Tests/TypeParserVDTests/TypeParserVDTests.swift
@@ -32,7 +32,11 @@ final class TypeParserVDTests: XCTestCase {
     func manageModel() throws {
         let data = Data(usersJson.utf8)
         users = try TypeParserVD.getData(from: data)
-        
+    }
+    
+    func manageModelWithResult<T: Codable>() -> Result<T, Error> {
+        let data = Data(usersJson.utf8)
+        return TypeParserVD.getData(from: data)
     }
     
     func test_givenValidJSON_completionWithUsers() throws {
@@ -42,13 +46,27 @@ final class TypeParserVDTests: XCTestCase {
         XCTAssertEqual(users.first, firstUser)
     }
     
+    func test_givenValidJSONWithResult_completionWithUsers() {
+        /// Given
+        let result: Result<[User], Error> = manageModelWithResult()
+        switch result {
+        case .success(let users):
+            self.users = users
+        case .failure(_):
+            XCTAssertTrue(false)
+        }
+        XCTAssertTrue(users.count > 0)
+        XCTAssertEqual(users.first, firstUser)
+    }
+    
     func test_givenMissingValueJSON_completionWithError() {
         
         do {
             let data = Data(missingValueUsersJson.utf8)
             users = try TypeParserVD.getData(from: data)
-        } catch let error {
-            if error is ParseError { XCTAssertEqual(error as! ParseError, .missingProperty)}
+        } catch {
+            print(error.localizedDescription)
+            XCTAssertEqual(error.localizedDescription, "The data couldn’t be read because it is missing.")
         }
     }
     
@@ -57,11 +75,33 @@ final class TypeParserVDTests: XCTestCase {
         do {
             let data = Data(invalidTypeUsersJson.utf8)
             users = try TypeParserVD.getData(from: data)
-        } catch let error {
-            if error is ParseError { XCTAssertEqual(error as! ParseError, .invalidType) }
+        } catch {
+            XCTAssertEqual(error.localizedDescription, "The data couldn’t be read because it isn’t in the correct format.")
         }
     }
     
+    func test_getDataWithResult_givenMissingValueJSON_completionWithError() {
+        let dataMissing = Data(missingValueUsersJson.utf8)
+        let resultMissing: Result<[User], Error> = TypeParserVD.getData(from: dataMissing)
+        switch resultMissing {
+        case .success(_):
+            XCTAssertTrue(false)
+        case .failure(let errorMissing):
+            print(errorMissing.localizedDescription)
+            XCTAssertEqual(errorMissing.localizedDescription, "The data couldn’t be read because it is missing.")
+        }
+    }
+    
+    func test_getDataWithResult_invalidTypeValueJSON_completionWithError() {
+        let dataInvalid = Data(invalidTypeUsersJson.utf8)
+        let resultType: Result<[User], Error> = TypeParserVD.getData(from: dataInvalid)
+        switch resultType {
+        case .success(_):
+            XCTAssertTrue(false)
+        case .failure(let errorParsing):
+            XCTAssertEqual(errorParsing.localizedDescription, "The data couldn’t be read because it isn’t in the correct format.")
+        }
+    }
 }
 
 let usersJson = """
