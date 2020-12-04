2820d1c486d70e3d1869c63f4ee4ef67eec9b1bc
Victor Capilla <victorcapilladeveloper@gmail.com>
2020-12-04 20:17:30 +0100

    Add ParseError Enum

---
+++ Sources/TypeParserVD/ParseError.swift
@@ -0,0 +1,13 @@
+//
+//  ParseError.swift
+//  
+//
+//  Created by Victor Capilla Developer on 4/12/20.
+//
+
+import Foundation
+
+enum ParseError: Int {
+    case missingProperty = 4865
+    case invalidType = 4864
+}
